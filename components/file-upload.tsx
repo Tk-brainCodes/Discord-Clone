d7af638616a695e49f78cbf431728e3d90784ebd
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-12-31 02:34:37 +0100

    feat: created modal for server creation and image upload. Added the uploadthing library for image support upload to the cloud

---
+++ components/file-upload.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { X } from "lucide-react";
+import Image from "next/image";
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProp {
+  endpoint: "serverImage" | "imageFile";
+  value: string;
+  onChange: (url?: string) => void;
+}
+
+const FileUpload = ({ endpoint, value, onChange }: FileUploadProp) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className='relative h-20 w-20'>
+        <Image
+          fill
+          loading='lazy'
+          src={value}
+          alt='upload'
+          className='rounded-full'
+        />
+        <button
+          onClick={() => onChange("")}
+          className='bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm'
+        >
+          <X className='h-4 w-4' />
+        </button>
+      </div>
+    );
+  }
+  return (
+    <UploadDropzone
+      //@ts-ignore
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
+
+export default FileUpload;
