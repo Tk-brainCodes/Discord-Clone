3d4b384c9d709617e4d104c619ed417a6278a38c
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-11 15:58:48 +0100

    feat: added creat channel modal and leave channel modal to the modal context

--- components/providers/modal-providers.tsx
+++ components/providers/modal-providers.tsx
@@ -3,8 +3,9 @@
 import CreateServerModal from "@/components/modals/create-server-modal";
 import InviteModal from "@/components/modals/invite-modal";
 import EditServerModal from "@/components/modals/edit-server-modal";
-import ManageMembers from "@.components/modals/manage-members";
+import ManageMembers from "@/components/modals/manage-members";
 import CreateChannelModal from "@/components/modals/create-channel";
+import LeaveChannel from "@/components/modals/leave-channel";
 import { useState, useEffect } from "react";
 
 export const ModalProvider = () => {
@@ -25,6 +26,7 @@ export const ModalProvider = () => {
       <EditServerModal />
       <ManageMembers />
       <CreateChannelModal />
+      <LeaveChannel />
     </>
   );
 };
