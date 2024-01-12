eb876a7783d11f528db8ad2bd4df7301f92a3950
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-12 04:19:34 +0100

    feat: added delete api route for deleting server, where the user must be admin

--- app/api/servers/[serverid]/route.ts
+++ app/api/servers/[serverid]/route.ts
@@ -7,6 +7,35 @@ interface ReqServerProps {
   imageUrl: string;
 }
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverid: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.serverid) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverid,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { serverid: string } }
