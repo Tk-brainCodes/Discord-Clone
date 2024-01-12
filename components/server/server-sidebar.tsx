69b605e78a6a79c9761b0468e5200b370d952800
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-12 11:22:00 +0100

    feat: added data for icons, audio, video and text channels

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -3,13 +3,30 @@ import React from "react";
 import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Hash, Mic, Video, ShieldAlert, ShieldCheck } from "lucide-react";
 
 import ServerHeader from "./server-header";
+import ServerSearch from "./server-search";
 
 interface ServerSideProps {
   serverId: string;
 }
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className='mr-2 w-4 h-4' />,
+  [ChannelType.AUDIO]: <Mic className='mr-2 w-4 h-4' />,
+  [ChannelType.VIDEO]: <Video className='mr-2 w-4 h-4' />,
+};
+
+const roleIcon = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className='mr-2 w-4 h-4 text-indigo-500' />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className='mr-2 w-4 h-4 text-rose-500' />,
+};
 
 const SeverSidebar = async ({ serverId }: ServerSideProps) => {
   const profile = await currentProfile();
@@ -43,18 +60,15 @@ const SeverSidebar = async ({ serverId }: ServerSideProps) => {
   const textChannel = server?.channels.filter(
     (channel) => channel.type === ChannelType.TEXT
   );
-
   //filter all the channels relating to video
   const videoChannel = server?.channels.filter(
     (channel) => channel.type === ChannelType.VIDEO
   );
-
   //filter all the channels relating to audio
   const audioChannel = server?.channels.filter(
     (channel) => channel.type === ChannelType.AUDIO
   );
-
-  const memebers = server?.members.filter(
+  const members = server?.members.filter(
     (member) => member.profileId !== profile.id
   );
 
@@ -69,6 +83,50 @@ const SeverSidebar = async ({ serverId }: ServerSideProps) => {
   return (
     <div className='flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]'>
       <ServerHeader server={server} role={role} />
+      <ScrollArea className='flex-1 px-3'>
+        <div className='mt-2'>
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannel?.map((channel) => ({
+                  icon: iconMap[channel.type],
+                  name: channel.name,
+                  id: channel.id,
+                })),
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: audioChannel?.map((channel) => ({
+                  icon: iconMap[channel.type],
+                  name: channel.name,
+                  id: channel.id,
+                })),
+              },
+              {
+                label: "Video Channels",
+                type: "channel",
+                data: videoChannel?.map((channel) => ({
+                  icon: iconMap[channel.type],
+                  name: channel.name,
+                  id: channel.id,
+                })),
+              },
+              {
+                label: "Members",
+                type: "member",
+                data: members?.map((member) => ({
+                  icon: roleIcon[member.role],
+                  name: member.profile.name,
+                  id: member.id,
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 };
