026736d822a9b9eb5e6177ce07c5237804622aa1
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-15 09:48:52 +0100

    feat: server channel component

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useParams, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 import { Channel, Server, MemberRole, ChannelType } from "@prisma/client";
-import { Hash, Mic, Video } from "lucide-react";
+import { Hash, Mic, Video, Trash, Edit, Lock } from "lucide-react";
+import ActionTooltip from "@/components/action-tooltip";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -20,8 +22,43 @@ const iconMap = {
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
   const params = useParams();
   const router = useRouter();
+  const Icon = iconMap[channel.type];
 
-  return <div>ServerChannel</div>;
+  return (
+    <button
+      className={cn(
+        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
+      )}
+    >
+      <Icon className='flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400' />
+      <p
+        className={cn(
+          "line-clamp-1 font-semibold text-sm text-zinc-500  group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+          params?.channelId === channel.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}
+      >
+        {channel.name}
+      </p>
+
+      {channel.name === "general" && (
+        <div className='ml-auto flex items-center gap-x-2'>
+          <Lock className='block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-600 dark:hover:text-zinc-300 transition' />
+        </div>
+      )}
+      {channel.name !== "general" && role !== MemberRole.GUEST && (
+        <div className='ml-auto flex items-center gap-x-2'>
+          <ActionTooltip label='Edit'>
+            <Edit className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-600 dark:hover:text-zinc-300 transition' />
+          </ActionTooltip>
+          <ActionTooltip label='Delete'>
+            <Trash className='hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-600 dark:hover:text-zinc-300 transition' />
+          </ActionTooltip>
+        </div>
+      )}
+    </button>
+  );
 };
 
 export default ServerChannel;
