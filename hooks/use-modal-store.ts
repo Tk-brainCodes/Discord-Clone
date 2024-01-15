d79142e9bbc1cea602d7b4f35a3441ca2e4c51d4
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-15 13:51:59 +0100

    feat: added "deleteServer", "deleteChannel", and "editChannel" type

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,4 +1,4 @@
-import { ChannelType, Server } from "@prisma/client";
+import { ChannelType, Server, Channel } from "@prisma/client";
 import { create } from "zustand";
 
 export type ModalType =
@@ -8,10 +8,13 @@ export type ModalType =
   | "manageMembers"
   | "createChannel"
   | "leaveServer"
-  | "deleteServer";
+  | "deleteServer"
+  | "deleteChannel"
+  | "editChannel";
 
 interface ModalData {
   server?: Server;
+  channel?: Channel;
   channelType?: ChannelType;
 }
 
