7fb0e4c8265883eedfb92c13839cca5db4d5ab6f
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-24 21:49:47 +0100

    feat: server id page

--- app/(main)/(routes)/server/[serverid]/page.tsx
+++ app/(main)/(routes)/server/[serverid]/page.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
-const ServerIdPage = () => {
-  return <div>ServerPage</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+
+interface SeverIdPageProps {
+  params: {
+    serverid: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: SeverIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverid,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/server/${params.serverid}/channels/${initialChannel?.id}`);
 };
 
 export default ServerIdPage;
