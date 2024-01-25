b0313bf8ab68d70e8461f3ce26695691d0cee483
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-25 11:03:17 +0100

    feat:  chat header component

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,12 +1,13 @@
 import { Hash } from "lucide-react";
 
 import MobileToggle from "@/components/mobile-toggle";
+import UserAvatar from "@/components/user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
   name: string;
   type: "channel" | "conversation";
-  imageUrl?: string;
+  imageUrl: string;
 }
 
 const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
@@ -15,6 +16,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
       <MobileToggle serverid={serverId} />
       {type === "channel" && (
         <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className='h-5 w-5 md:w-5 mr-2' />
       )}
       <p className='font-semibold text-md text-blakc dark:text-white'>{name}</p>
     </div>
