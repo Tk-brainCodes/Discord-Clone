412c2520a9c1ec5be1d84abd9d0f2aacc978475b
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-28 23:33:28 +0100

    feat: added the chat input component for displaying chat input with props `name, type, apiUrl, query`

--- app/(main)/(routes)/server/[serverid]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/server/[serverid]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -43,6 +44,13 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         serverId={channel?.serverId}
         type='channel'
         imageUrl=''
+      />
+      <div className='flex-1'>Feauture Messages</div>
+      <ChatInput
+        name={channel.name}
+        type='channel'
+        apiUrl='/api/socket/messages'
+        query={{ channelId: channel.id, serverId: channel.serverId }}
       />
     </div>
   );
