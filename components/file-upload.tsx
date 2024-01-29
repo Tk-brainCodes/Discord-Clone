da2e2545afb5a0ee1d2cc2f23678e20e9da5b08e
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-29 09:42:21 +0100

    chore: changed `imageFile` to `messageFile`

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { X } from "lucide-react";
+import { X, FileIcon } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 
 interface FileUploadProp {
-  endpoint: "serverImage" | "imageFile";
+  endpoint: "serverImage" | "messageFile";
   value: string;
   onChange: (url?: string) => void;
 }
@@ -28,6 +28,28 @@ const FileUpload = ({ endpoint, value, onChange }: FileUploadProp) => {
         <button
           onClick={() => onChange("")}
           className='bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm'
+        >
+          <X className='h-4 w-4' />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className='relative flex items-center p-2 mt-2 rounded-md bg-background/10'>
+        <FileIcon className='h-10 w-10 fill-indigo-200 stroke-indigo-400' />
+        <a
+          href={value}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='mt-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline'
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className='bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm'
         >
           <X className='h-4 w-4' />
         </button>
