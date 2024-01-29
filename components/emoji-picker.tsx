001f76cc8115dcb7b4ef924c0a690865522a3cef
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-01-29 12:14:18 +0100

    feat: emoji picker component

---
+++ components/emoji-picker.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Smile } from "lucide-react";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+import { useTheme } from "next-themes";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface EmojiPickerProps {
+  onChange: (value: string) => void;
+}
+
+const EmojiPicker = ({ onChange }: EmojiPickerProps) => {
+  const { resolvedTheme } = useTheme();
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Smile className='text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300' />
+      </PopoverTrigger>
+      <PopoverContent
+        side='right'
+        sideOffset={40}
+        className='bg-transparent border-none shadow-sm drop-shadow-none mb-16'
+      >
+        <Picker
+          theme={resolvedTheme}
+          data={data}
+          onEmojiSelect={(emoji: any) => onChange(emoji.native)}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default EmojiPicker;
