e00f9a1dac4d9aee3a616c1b7ba402c1ff9f5010
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-02-05 00:25:49 +0100

    feat: direct message route for deleting a specific message

---
+++ app/api/direct-messages/route.ts
@@ -0,0 +1,79 @@
+import { NextResponse } from "next/server";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { DirectMessage } from "@prisma/client";
+
+const MESSAGE_BATCH = 10;
+
+export async function GET(req: Request) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+    const cursor = searchParams.get("cursor");
+    const conversationId = searchParams.get("conversationId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!conversationId) {
+      return new NextResponse("Conversation ID Missing", { status: 400 });
+    }
+
+    let messages: DirectMessage[] = [];
+
+    if (cursor) {
+      messages = await db.directMessage.findMany({
+        take: MESSAGE_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor,
+        },
+        where: {
+          conversationId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } else {
+      messages = await db.directMessage.findMany({
+        take: MESSAGE_BATCH,
+        where: {
+          conversationId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }
+
+    let nextCursor = null;
+
+    if (messages.length === MESSAGE_BATCH) {
+      nextCursor = messages[MESSAGE_BATCH - 1].id;
+    }
+
+    return NextResponse.json({
+      items: messages,
+      nextCursor,
+    });
+  } catch (error) {
+    console.log("[DIRECT_MESSAGES_ERROR]", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
