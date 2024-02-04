6d34a4171f73fa6c8f33e11121f163147ad18cbe
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-02-05 00:28:25 +0100

    feat: added the chat video button to turn on or off video/audio conversations

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -3,6 +3,7 @@ import { Hash } from "lucide-react";
 import MobileToggle from "@/components/mobile-toggle";
 import UserAvatar from "@/components/user-avatar";
 import { SockeIndicator } from "@/components/socket-indicator";
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -24,6 +25,7 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
       <p className='font-semibold text-md text-blakc dark:text-white'>{name}</p>
 
       <div className='ml-auto flex items-center'>
+        {type === "conversation" && <ChatVideoButton />}
         <SockeIndicator />
       </div>
     </div>
